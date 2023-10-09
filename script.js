97a2c5c62ada35f3f4c700f9258cd5bc72dda4a2
Vivian Wang <VivianW@whitecliffe.ac.nz>
2023-10-10 11:06:02 +1300

    adding senarios

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function generateScenario() {
     const scenarios = [
         "Low-Achieving Students' Struggles",
-        "Lack of Teacher Support",
         "Financial Difficulties",
         "Physical and Psychological Readiness",
         "Loss of Hands-on Experiences",
@@ -14,24 +13,20 @@ function generateScenario() {
 function generateEmail(scenario) {
     const emails = {
         "Low-Achieving Students' Struggles": [
-            "Hi professor,\n\nI'm having some difficulties understanding the assignment instructions. Could you please provide some clarification? Thank you.",
-            "Hello,\n\nI'm struggling a bit with the assignment. Can you help me understand it better? Appreciate your assistance.",
-            "Hey,\n\nI'm finding it hard to grasp the assignment details. Any chance you could explain it in simpler terms?"
+            "Amy, a 25-year-old female student, enrolled in your online course. She submitted the first assignment just meeting the passing grade. However, she has become very quiet and her second assignment is now two days overdue.\n\nTips: In this email, it's important to express understanding and concern for Amy's situation. Additionally, inform her about the college's policy regarding late submissions. Offer support and encourage her to communicate any challenges she may be facing.",
+            "Sam, an 18-year-old student, are enrolled in your online course. They missed the deadline for the first assignment but submitted it two days later. Unfortunately, their submission was incomplete, as they missed half of the assignment. The first half, while completed, falls within the passing range. Your task is to compose an email to Sam, encouraging them to resubmit the assignment and offering guidance on how to improve their next submission.\n\nTips: In this email, it's crucial to address Sam's situation with empathy and encouragement. Clearly communicate the issues with their previous submission and emphasize the importance of a complete assignment. Offer specific suggestions on how they can improve their work for the next submission. Remember to provide resources or support options that can aid Sam in their efforts to succeed.",
+            "Alex, a 20-year-old male student, is enrolled in your online course. He has been consistently submitting assignments on time, but the performance has been below average. Their recent quiz results indicate a struggle with understanding the material. Tips: In this email, it's important to express concern about Alex's performance and offer support. Encourage them to seek help, such as attending virtual office hours or utilizing additional resources. Consider providing specific suggestions for improvement and highlight the importance of regular engagement with the course material."
         ],
-        "Lack of Teacher Support": [
-            "Hi professor,\n\nI feel like I'm not getting enough support in the online classes. Can you suggest some strategies to help me out?",
-            "Hello,\n\nI've been feeling a bit lost in the online classes. Can you provide some guidance or resources to help me catch up?",
-            "Hey,\n\nI'm struggling with the online format and could use some more support. Do you have any advice or tips?"
-        ],
+
         "Financial Difficulties": [
             "Hi professor,\n\nI'm facing some financial challenges which are affecting my ability to access the required course materials. Any suggestions?",
             "Hello,\n\nI'm having some financial difficulties right now. How can I make sure I can still access all the necessary resources for the course?",
             "Hey,\n\nI'm worried about my finances, and it's impacting my access to course materials. Can you offer any advice or alternatives?"
         ],
         "Physical and Psychological Readiness": [
-            "Hi professor,\n\nI'm feeling a bit unprepared for the demands of online learning. Can you share some tips on how to get better prepared?",
-            "Hello,\n\nI'm not sure if I'm physically and mentally ready for online learning. Any advice on how to handle this transition?",
-            "Hey,\n\nI'm feeling a bit overwhelmed by the shift to online learning. Can you suggest some ways to better prepare myself?"
+            "Sophia, a 32-year-old student with a history of anxiety, recently started your online course. She's struggling with the isolation of online learning and expresses concerns about her mental well-being. Sophia is seeking resources and support to help her navigate these challenges.\n\nTips: Provide Sophia with information about available mental health resources, both within your organization and externally. Encourage her to establish a routine that includes self-care practices and social connections, even in a digital environment.",
+            "Alex, a 23-year-old student and a part-time fitness instructor, is enrolled in your online course. They're experiencing difficulties in adjusting to the sedentary nature of online learning after being used to an active lifestyle. They're seeking strategies to maintain physical health while pursuing their studies.\n\nTips: Offer Alex tailored advice on incorporating short, effective workouts into their routine, as well as tips on maintaining a healthy posture during extended study sessions.",
+            "Emily, a 28-year-old student and a single parent of two young children, recently joined your online course. She expresses feeling overwhelmed by the responsibilities of parenting while trying to focus on her studies. She's seeking advice on how to create a balanced routine.\n\nTips: In this email, acknowledge Emily's unique situation and provide practical tips on time management, setting priorities, and finding moments of self-care amidst her responsibilities."
         ],
         "Loss of Hands-on Experiences": [
             "Hi professor,\n\nI'm in a discipline that usually involves hands-on learning. I'm worried about losing out on practical skills. Any suggestions?",
@@ -39,8 +34,8 @@ function generateEmail(scenario) {
             "Hey,\n\nI'm in a discipline where hands-on learning is crucial. How can I ensure I'm not missing out on practical skills during online classes?"
         ],
         "Unequal Access to Education": [
-            "Hi professor,\n\nI come from a disadvantaged background and face challenges in accessing quality online education. Any advice on how to level the playing field?",
-            "Hello,\n\nI'm dealing with some challenges in accessing quality online education due to my background. Can you suggest some ways to overcome this?",
+            "Isabella, a 20-year-old student from a rural area, recently joined your online course. She expresses concerns about the limited access to high-speed internet and technology in her area, which is affecting her ability to participate fully in the course. Isabella is seeking advice on how to overcome these access challenges.\n\nTips: In your response, acknowledge Isabella's situation and offer alternative solutions for accessing course materials and participating in online activities with limited internet connectivity. Provide guidance on how to optimize learning with the available resources, and suggest local or community resources that may provide additional support. Additionally, consider offering alternative assignments or assessments that don't heavily rely on online quizzes.",
+            "Alex, a dedicated student with a slower laptop, recently encountered challenges while attempting to complete a written assignment. They describe experiencing delays and difficulties in accessing online resources. Alex is looking for strategies to optimize their laptop's performance for smoother online learning.\n\nTips: Advise Alex to close unnecessary applications, clear browser cache, and consider upgrading memory or storage if possible. Additionally, recommend utilizing offline resources or downloading essential course materials in advance. Also, advise that local libraries provide free usage of laptops and internet.",
             "Hey,\n\nI'm facing some barriers in accessing quality online education. What steps can I take to ensure I have an equal opportunity to succeed?"
         ]
     };
